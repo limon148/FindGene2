9230b8260a91e9263ff85a1137ff55db66523fc3
limon_148 <mehadihasan.cseru@gmail.com>
2017-08-26 12:47:59 -0700

    this method return the gene formed from the “ATG” and the closest stop codon that is a multiple of three away. If there is no valid stop codon and therefore no gene, return the empty string.

--- Part1.java
+++ Part1.java
@@ -19,12 +19,28 @@ public class Part1
         }
         return dna.length();
     }
-    public void testFindStopCodon(){
-        String dna = "ATGATCTAATTTATGCTGCAACGGTGAAGA";
+    public String findGene(String dna){
         int startIndex = dna.indexOf("ATG");
-        String stopCodon = "TAA";
-        System.out.println(findStopCodon(dna, startIndex, stopCodon));
-        dna = "";
-        System.out.println(findStopCodon(dna, startIndex, stopCodon));
+        if(startIndex == -1){
+            return "";
+        }
+        int taaIndex = findStopCodon(dna, startIndex, "TAA");
+        int tgaIndex = findStopCodon(dna, startIndex, "TGA");
+        int tagIndex = findStopCodon(dna, startIndex, "TAG");
+        int minIndex = 0;
+        if (taaIndex == -1 || (tgaIndex != -1 && tgaIndex < taaIndex)) {
+            minIndex = tgaIndex;
+        }
+        else {
+            minIndex = taaIndex;
+        }
+        if (minIndex == -1 || (tagIndex != -1 && tagIndex < minIndex)) {
+            minIndex = tagIndex;
+        }
+        if (minIndex == -1){
+            return "";
+        }
+        return dna.substring(startIndex, minIndex + 3);
     }
+    
 }
