90220e1dc6ef2da5d5cc4f9dbf961a611ecf1451
limon_148 <mehadihasan.cseru@gmail.com>
2017-08-26 14:39:20 -0700

    The howMany method returns an integer indicating how many times stringa appears in stringb, where each occurrence of stringa must not overlap with another occurrence of it.

---
+++ Part2.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Part2 here.
+ *
+ * @author (Mehedi Hasan)
+ * @version (27-18-2017)
+ */
+public class Part2
+{
+    public int howMany(String stringa, String stringb){
+        int startIndex = 0,count = 0;
+        int curIndex = stringb.indexOf(stringa, startIndex);
+        while(true){
+            if(curIndex != -1){
+                count++;
+            }
+            else{
+                break;
+            }
+            curIndex = stringb.indexOf(stringa, curIndex + stringa.length());
+        }
+        return count;
+    }
+}
